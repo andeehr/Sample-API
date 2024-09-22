d576fff0abf06281c754d1a75bf6c89bc6f38e68
Andres Mora <andeehr@gmail.com>
2024-09-22 18:38:28 -0300

    update dependencies and add simple test

---
+++ Sample.Test/Services/UserServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using AutoFixture.Xunit2;
+using AutoMapper;
+using Moq;
+using Sample.Common.DTOs.Requests;
+using Sample.Core.Services;
+using Sample.Core.Services.Interfaces;
+using Sample.Data.Entities;
+using Sample.Data.Persistence.Interfaces;
+using Sample.Test.Utils;
+using Xunit;
+
+namespace Sample.Test.Services
+{
+    public class UserServiceTests
+    {
+        [Theory()]
+        [DefaultData()]
+        public async Task AddAsync_HappyCase_ShouldBeOk(
+            UserRequest request,
+            [Frozen] Mock<IMapper> mockMapper,
+            [Frozen] Mock<IUserRepository> mockUserRepository,
+            [Frozen] Mock<IValidatorService> mockValidatorService,
+            UserService sut)
+        {
+            // Act
+            await sut.RegisterAsync(request);
+
+            // Assert
+            mockMapper.Verify(r => r.Map<User>(request), Times.Once);
+            mockValidatorService.Verify(r => r.Validate(request), Times.Once);
+            mockUserRepository.Verify(r => r.AddAsync(It.IsAny<User>(), It.IsAny<bool>()), Times.Once);
+        }
+    }
+}
---
+++ Sample.Test/Utils/DefaultDataAttribute.cs
@@ -0,0 +1,51 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+
+namespace Sample.Test.Utils
+{
+    public class DefaultDataAttribute : AutoDataAttribute
+    {
+        public DefaultDataAttribute()
+            : base(FixtureFactory([]))
+        {
+        }
+
+        public DefaultDataAttribute(params Type[] customizationTypes)
+            : base(FixtureFactory(customizationTypes ?? []))
+        {
+        }
+
+        private static Func<IFixture> FixtureFactory(Type[] customizationTypes)
+        {
+            return delegate
+            {
+                var fixture = new Fixture();
+
+                fixture.Customize(new CompositeCustomization(
+                    new ICustomization[1]
+                    {
+                        new DefaultCustomization()
+                    }
+                    .Concat(customizationTypes.Select(t => (ICustomization)Activator.CreateInstance(t)!))
+                ));
+
+                fixture.Behaviors.OfType<ThrowingRecursionBehavior>()
+                    .ToList()
+                    .ForEach(b => fixture.Behaviors.Remove(b));
+
+                fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+                return fixture;
+            };
+        }
+
+        public class DefaultCustomization : CompositeCustomization
+        {
+            public DefaultCustomization()
+                : base(new AutoMoqCustomization())
+            {
+            }
+        }
+    }
+}
